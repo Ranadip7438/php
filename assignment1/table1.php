7ad441a657f5680934f576446ae45d1dd8e42f04
Ranadip7438 <mitrapaul10@gmail.com>
2026-08-31 10:17:07 +0530

    Add files via upload

---
+++ assignment1/table1.php
@@ -0,0 +1,31 @@
+<?php
+
+$name = "bama";
+$age = 21;
+$course = "BCA";
+
+echo "<table border='1' cellpadding='8'>";
+
+echo "<tr>
+        <th>Field</th>
+        <th>Value</th>
+      </tr>";
+
+echo "<tr>
+        <td>Name</td>
+        <td>$name</td>
+      </tr>";
+
+echo "<tr>
+        <td>Age</td>
+        <td>$age</td>
+      </tr>";
+
+echo "<tr>
+        <td>Course</td>
+        <td>$course</td>
+      </tr>";
+
+echo "</table>";
+
+?>
